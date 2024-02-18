4c87d95c722cb6cfd152a945c6432af100353960
Jl115 <joelbern006@gmail.com>
2024-02-18 22:27:42 +0100

    making the last exam

---
+++ functions_compositions.js
@@ -0,0 +1,8 @@
+const sentence =
+  "PechaKucha is a presentation style in which 20 slides are shown for 20 seconds each (6 minutes and 40 seconds in total).";
+
+const R = require("./rambda.min");
+
+// 1 filter for nummers 
+// 2 array length 
+// compose those two
